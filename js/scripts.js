3d7bf828122fa4114281cf284f466c32d767790e
Jimmy Zebroski <zebrozkii@gmail.com>
2019-01-25 08:51:13 -0800

    base index is set up and linked script to html, created a for loop set to number person enters

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    var number = parseInt($("#userInput").val());
+    for(var loopAmount=0;loopAmount<=number;loopAmount++){
+      
+
+
+    }
+  });
+});
