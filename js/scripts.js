5dc401b252603c5b562499cbdff053cfe508a14a
Jimmy Zebroski <zebrozkii@gmail.com>
2019-01-25 11:53:05 -0800

    Struggling to get output correct experimenting with different outputs but all statements wworking

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,20 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var number = $("#userInput").val();
+    var length = parseInt($("#userInput").val());
     if(number!=0){
       var input = number.split("");
-      for (var index = 0; index < input.length; index++) {
+      for (var index = 0; index < length; index++) {
         if(input[index]==="1"){
-        alert("boop");
-      }else{
-
+        input[index]=("boop");
+        }else if (input[index]%3===0) {
+          input[index]=("Sorry I cant do that");
+        }
+        $("#results").append(input);
       }
-    }
     }else{
-  alert("beep");
-}
+      input=("beep");
+    }
+
   });
 });
